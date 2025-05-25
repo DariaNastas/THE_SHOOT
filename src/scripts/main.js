b9ba169994bb09305d252b11f32208546a183f8e
Daria Nastas <bycinska@gmail.com>
2025-05-25 21:59:57 +0300

    add task solution_25.05.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('THE SHOOT is ready!');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("userMenuToggle");
+  const dropdown = document.getElementById("userMenuDropdown");
+
+  toggleButton.addEventListener("click", () => {
+    dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!toggleButton.contains(e.target) && !dropdown.contains(e.target)) {
+      dropdown.style.display = "none";
+    }
+  });
+});
